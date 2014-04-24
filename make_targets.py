8eab9e449f89708baa30f5f8e73514dbe54d3037
Luis Armendariz <luis.armendariz@gmail.com>
2014-04-24 13:09:22 -0700

    Updated make_targets.py

--- make_targets.py
+++ make_targets.py
@@ -3,16 +3,16 @@
 import sys, os
 from os.path import exists, dirname, basename, splitext
 
-def usage():
-    sys.stderr.write('Usage: {0} file1.cpp file2.cpp [...]\n'.format(sys.argv[0]))
-    sys.exit(1)
-    
 def main():
+
     args = sys.argv[1:]
+
     if len(args) == 0:
-        usage()
-    
+        sys.stderr.write('Usage: {0} file1.cpp file2.cpp [...]\n'.format(sys.argv[0]))
+        sys.exit(1)
+
     sources = [path for path in args if exists(path) and path.endswith('.cpp')]
+
     with open('targets.ninja', 'w') as fp:
         fp.write('# Created by {0}\n'.format(sys.argv[0]))
         fp.write('# THIS FILE WILL BE OVERWRITTEN!\n')
@@ -28,7 +28,10 @@ def main():
             fp.write('build $builddir/{target}.o: cxx {src}\n'.format(**d))
             fp.write('build $bindir/{target}: link $builddir/{target}.o\n'.format(**d))
             print('Added build rule for bin/{target}'.format(**d))
+        fp.write('\n# EOF\n')
         print('Wrote {0}'.format(fp.name))
+
+    return
 
 if __name__ == '__main__':
     main()
